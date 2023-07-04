ef47c2c595d088fdc7693848eddc11ae60915ba0
sergio alvarez pinto <sertech_live@hotmail.com>
2023-07-04 12:13:35 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function userScroll() {
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add("bg-dark");
+            navbar.classList.add("navbar-sticky");
+        } else {
+            navbar.classList.remove("bg-dark");
+            navbar.classList.remove("navbar-sticky");
+        }
+    });
+}
+
+// we want to run this when the document is loaded
+document.addEventListener("DOMContentLoaded", userScroll());
